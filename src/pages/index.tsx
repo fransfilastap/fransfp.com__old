d585590c94885da034e8f1d7bf80ec62ae8d0e27
Frans Filasta Pratama <fransfilastap@live.com>
2021-12-05 10:13:29 +0700

    Changes Index Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,10 +13,10 @@ export default function Index() {
       <div className="container">
         <div>
           <h1>
-            Hi, We're Next.js & Netlify<span className="fancy">.</span>
+            Hi, I'm Frans Filasta Pratama<span className="fancy">.</span>
           </h1>
-          <span className="handle">@nextjs-netlify-blog</span>
-          <h2>A blog template with Next.js and Netlify.</h2>
+          <span className="handle">@fransfp__</span>
+          <h2>Software engineer, and Civil Servant too 😅</h2>
           <SocialList />
         </div>
       </div>
