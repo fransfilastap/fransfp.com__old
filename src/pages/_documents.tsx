ec352cd1d1398c178b63baea68ad72a455de9a41
Frans Filasta Pratama <fransfilastap@live.com>
2022-01-10 08:57:23 +0700

    Fix window.gtag doesn't exists

--- src/pages/_documents.tsx
+++ src/pages/_documents.tsx
@@ -1,6 +1,12 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document'
+import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document'
 
 export default class MyDocument extends Document {
+
+    static async getInitialProps(ctx: DocumentContext) {
+        const initialProps = await Document.getInitialProps(ctx)
+        return { ...initialProps }
+    }
+
     render() {
         return (
             <Html>
@@ -11,14 +17,14 @@ export default class MyDocument extends Document {
                     />
                     <script
                         dangerouslySetInnerHTML={{
-                            __html: `
-                            window.dataLayer = window.dataLayer || [];
-                            function gtag(){dataLayer.push(arguments);}
-                            gtag('js', new Date());
-                            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}',{
-                                page_path: window.location.pathname,
-                                }
-                            );`,
+                        __html: `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                        page_path: window.location.pathname,
+                        });
+                    `,
                         }}
                     />
                 </Head>
